aee6c71f68c4f89125ff48b801e0b51779020739
gcranechem <gcranechem@gmail.com>
2024-12-13 18:21:40 +0800

    修改pm2

--- run.py
+++ run.py
@@ -10,9 +10,6 @@ from app import create_app
 # Add the myapp directory to sys.path (use absolute path)
 sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
 
-# 启动 FastAPI 服务
-subprocess.Popen(["python", "services/langserve_service.py"])
-
 app = create_app()
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
